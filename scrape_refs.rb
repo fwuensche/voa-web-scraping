c9ab0ce6e9a46eebc80cb57744e5a91d4b7c2e6b
Flavio Wuensche <f.wuensche@gmail.com>
2016-01-07 01:53:02 +0000

    Scrape properties' reference and url

---
+++ scrape_refs.rb
@@ -0,0 +1,32 @@
+# open the website
+require 'mechanize'
+agent = Mechanize.new
+page = agent.get('http://www.2010.voa.gov.uk/rli/en/basic/find')
+
+# fill the search form
+form = page.forms.first
+form['searchTerm'] = 'sw16'
+results_page = form.submit
+
+results = []
+File.write('refs_and_urls.csv', '')
+next_link = results_page.link_with(text: '»')
+
+count = 0
+begin
+    # go to the next page
+    results_page = next_link.click
+
+    # get the results
+    trs = results_page.search('tbody tr')
+    trs.each do |tr|
+        bar = tr.search('td.first').text
+        url = tr.search('a').first['href']
+        open('refs_and_urls.csv', 'a') { |f| f.puts "#{bar},#{url}" }
+    end
+    next_link = results_page.link_with(text: '»')
+    p count+=1 if count%20 == 0
+    #sleep(1)
+end until next_link.nil?
+
+p count
